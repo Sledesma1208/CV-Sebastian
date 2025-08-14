e612df1bc28c87c5bb81db007875d7324baf7531
Sebastian Ledesma <sebastanledesmamontoya12@gmail.com>
2025-08-14 15:19:37 -0500

    Feature: Contenido y animaciones finalizadas (posible final)

--- src/ProjectsSection/Projects-section.tsx
+++ src/ProjectsSection/Projects-section.tsx
@@ -1,55 +1,57 @@
-import { useState } from 'react'
-import './Projects-section.css'
+import type { MouseEvent } from "react";
+import "./Projects-section.css";
 
 function Projectsection() {
-  const [showAlert, setShowAlert] = useState(false)
-
-  const handleClick = () => {
-    setShowAlert(true)
-    setTimeout(() => {
-      setShowAlert(false)
-    }, 7500)
+  function handleClick(e: MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    alert(
+      "Los proyectos en estos momentos se encuentran en proceso de versionamiento público debido a que algunos quedaron en repositorios privados.\nSin embargo, puedes contactarme para más información."
+    );
   }
 
   return (
     <div className="ProjectsSection relative">
-      {showAlert && (
-        <div className="SuccessAlert">
-          Este proyecto fue desarrollado originalmente para una empresa privada y<br />
-           entregado como parte de sus operaciones internas. <br />
-          Actualmente me encuentro trabajando en una versión pública para su visualización.
-        </div>
-      )}
-
-      <div className='ProjectsTitle'>
-      <h1>Proyectos</h1>
+      <div className="ProjectsTitle">
+        <h1>PROYECTOS</h1>
       </div>
 
       <div className="Project1">
         <h1>Biblioteca Digital </h1>
         <span>• .Net(C#) & AngularJS •</span>
         <p>
-          App para gestionar libros, préstamos y usuarios de una biblioteca física y digital<br />
+          App para gestionar libros, préstamos y usuarios de una biblioteca
+          física y digital
         </p>
-        <a href="">Ver mas</a>
+        <a href="#" onClick={handleClick}>
+          Ver mas
+        </a>
       </div>
 
       <div className="Project2">
         <h1>Préstamos estudiantiles</h1>
         <span>• Django y ReactJS •</span>
-        <p>Créditos rápidos y accesibles para que los estudiantes aseguren educación.</p>
-        <a href="">Ver mas</a>
+        <p>
+          Créditos rápidos y accesibles para que los estudiantes aseguren
+          educación.
+        </p>
+        <a href="#" onClick={handleClick}>
+          Ver mas
+        </a>
       </div>
 
       <div className="Project3">
         <h1>Reserva de alojamientos</h1>
         <span>• Java y VueJS •</span>
-        <p>Plataforma para reservar hospedajes según tus necesidades y preferencias.</p>
-        <a href="">Ver mas</a>        
+        <p>
+          Plataforma para reservar hospedajes según tus necesidades y
+          preferencias.
+        </p>
+        <a href="#" onClick={handleClick}>
+          Ver mas
+        </a>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Projectsection
+export default Projectsection;
